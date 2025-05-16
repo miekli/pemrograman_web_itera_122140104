38a8b843007883b23a5839bb58d4611d1ea02d3b
miekli <128711146+miekli@users.noreply.github.com>
2025-05-16 23:15:56 +0700

    penambahan foto lupa dikirim dan menaruh 2 file terpisah ke 1 folder (salah ngepush tadi)

---
+++ Mychael Daniel N_122140104_Pertemuan6/tugas/manajemen/manajemen/scripts/initialize_db.py
@@ -0,0 +1,60 @@
+import argparse
+import sys
+
+from pyramid.paster import bootstrap, setup_logging
+from sqlalchemy.exc import OperationalError
+
+from .. import models
+
+
+def setup_models(dbsession):
+    """
+    Add initial model objects.
+    """
+    # Tambahkan data awal untuk Mahasiswa
+    matakuliah1 = models.MataKuliah(
+        kode_mk='CS101',
+        nama_mk='Algoritma dan Pemrograman',
+        sks=3,
+        semester=1
+    )
+    matakuliah2 = models.MataKuliah(
+        kode_mk='CS102',
+        nama_mk='Struktur Data',
+        sks=3,
+        semester=2
+    )
+    dbsession.add(matakuliah1)
+    dbsession.add(matakuliah2)
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'config_uri',
+        help='Configuration file, e.g., development.ini',
+    )
+    return parser.parse_args(argv[1:])
+
+
+def main(argv=sys.argv):
+    args = parse_args(argv)
+    setup_logging(args.config_uri)
+    env = bootstrap(args.config_uri)
+
+    try:
+        with env['request'].tm:
+            dbsession = env['request'].dbsession
+            setup_models(dbsession)
+    except OperationalError:
+        print('''
+Pyramid is having a problem using your SQL database.  The problem
+might be caused by one of the following things:
+
+1.  You may need to initialize your database tables with `alembic`.
+    Check your README.txt for description and try to run it.
+
+2.  Your database server may not be running.  Check that the
+    database server referred to by the "sqlalchemy.url" setting in
+    your "development.ini" file is running.
+            ''')
